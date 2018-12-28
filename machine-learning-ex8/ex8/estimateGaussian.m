1858ee6ce96cae6b0ba0a0c2d6e77ebc286856ca
nicole7han <nicole7han@gmail.com>
2018-12-28 10:54:56 -0800

    exe8

--- machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -21,9 +21,8 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
+mu = mean(X,1)';
+sigma2 = (sum((X-mean(X,1)).^2)/m)';
 
 
 
--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,13 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
-
-
-
-
-
-
-
-
-
-
+    predictions = (pval < epsilon);
+    tp = length(intersect(find(predictions==1),find(yval==1)));
+    fp = length(intersect(find(predictions==1),find(yval==0)));
+    fn = length(intersect(find(predictions==0),find(yval==1)));
+    prec = tp/(tp+fp);
+    rec = tp/(tp+fn);
+    F1 = 2*prec*rec/(prec+rec);
 
     % =============================================================
 
